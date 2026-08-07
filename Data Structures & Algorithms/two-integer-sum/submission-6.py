500bb9a797384ad8011b86ca4130cba92f7cbc33
Zachary Derival <zderival@gmail.com>
2026-08-06 21:28:41 -0400

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in hashmap.keys():
+                return [hashmap[complement],i]
+            hashmap[num] = i
+
+        for i in nums:
+            complement = target - i
+            if complement in hashmap.keys():
+                return [hashmap[i],hashmap[complement]]
+        """
+        Whenever i have to match a number to its indices, count or
+        vise versa, expect an hashmap
+
+        I can make a hashmap and set the key to the int and value to the index.
+        How do i find which two values in the hashmap = target without O(n^2)?
+        """
